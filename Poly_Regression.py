13013a2f141a8ca4a091b145ee10bd17d759fd7c
danielennis521 <125907715+danielennis521@users.noreply.github.com>
2024-11-15 18:16:29 -0600

    Update Poly_Regression.py

--- Poly_Regression.py
+++ Poly_Regression.py
@@ -4,19 +4,37 @@ import matplotlib.pyplot as plt
 
 
 class PolyLeastSquares():
-    def __init__(self, degree=1, learning_rate=1e-3, max_iterations=1000, tol=None):
+    def __init__(self, degree=1, learning_rate=2*1e-6, max_iterations=2*int(1e6), tol=None, momentum=0.2):
         self.degree = degree
         self.n = degree+1
         self.a = np.zeros(degree+1)
         self.learning_rate = learning_rate
         self.max_iterations = max_iterations
         self.tol = tol
+        self.gamma = momentum
         
 
-    def fit(self, x, y, type='normal'):
-        method = {'normal': self.fit_normal, 'gd': self.fit_gd}
-        return method[type](x, y)
+    def fit(self, x, y, method='normal'):
+        if self.tol is None:
+            self.tol = 1e-1 * la.norm(y)
 
+        v = np.zeros(self.n)
+        self.x = x
+        self.y = y
+        self.a = np.zeros(self.n)
+        self.m = len(x)
+        self.X = np.array([np.power(x, i) for i in range(self.n)]).T
+
+    
+
+        functions = {'normal': self.fit_normal
+                     , 'gd': self.fit_gd
+                     , 'momentum': self.fit_momentum
+                     , 'nesterov': self.fit_nesterov}
+
+        return functions[method](x, y)
+
+    # "exact" solution via normal equations
     def fit_normal(self, x, y):
         self.x = np.array(x)
         self.y = np.array(y)
@@ -30,32 +48,54 @@ class PolyLeastSquares():
 
         return self.a
 
+    # standard gradient descent
     def fit_gd(self, x, y):
-        if tol is None:
-            tol = 1e-2 * la.norm(y)
-
-        self.x = x
-        self.y = y
-        self.a = np.zeros(self.n)
-        self.m = len(x)
-        X = np.array([np.power(x, i) for i in range(self.n)]).T
         
         for i in range(self.max_iterations):
-            error = np.dot(X, self.a) - y
-            gradient = np.dot(X.T, error)
+            error = np.dot(self.X, self.a) - y
+            gradient = np.dot(self.X.T, error)
             self.a -= self.learning_rate * gradient * (1/self.m)
 
-            if la.norm(gradient) < tol or self.a[0] > 1e6:
+            if la.norm(gradient) < self.tol or self.a[0] > 1e6:
                 break
 
-        print(la.norm(gradient))
+        return self.a
+
+    # gradient descent with momentum
+    def fit_momentum(self, x, y):
+
+        for i in range(self.max_iterations):
+            error = np.dot(self.X, self.a) - y
+            gradient = np.dot(self.X.T, error)
+            v = self.gamma*v + self.learning_rate * gradient * ((1-self.gamma)/self.m)
+            self.a -= v
+
+            if la.norm(gradient) < self.tol or self.a[0] > 1e6:
+                break
+
+        return self.a
+
+    # Nesterov accelerated gradient descent
+    def fit_nesterov(self, x, y):     
+        
+        for i in range(self.max_iterations):
+            error = np.dot(self.X, self.a+self.gamma*v) - y
+            gradient = np.dot(self.X.T, error)
+            v = self.gamma*v + self.learning_rate * gradient * ((1-self.gamma)/self.m)
+            self.a -= v
+
+            if la.norm(gradient) < self.tol or self.a[0] > 1e6:
+                break
+
         return self.a
     
+
     def plot_Data(self, data_marker='ro'):
         plt.plot(self.x, self.y, data_marker)
         plt.show()
         plt.cla()
         return
+
 
     def plot_Fit(self, fit_marker='b'): 
         x = np.linspace(min(self.x), max(self.x), 100)
@@ -65,6 +105,7 @@ class PolyLeastSquares():
         plt.cla()
         return
     
+
     def plot_Both(self, data_marker='ro', fit_marker='b'):
         x = np.linspace(min(self.x), max(self.x), 100)
         y = np.sum([self.a[i]*x**i for i in range(self.n)], axis=0) 
@@ -72,4 +113,3 @@ class PolyLeastSquares():
         plt.show()
         plt.cla()       
         return
-    
