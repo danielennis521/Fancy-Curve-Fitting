73c9827df2840feb397af5b19d5b5ab7c3cdef74
danielennis521 <125907715+danielennis521@users.noreply.github.com>
2025-03-21 16:43:45 -0500

    Update Extended_Elastic_Net.py

--- Polynomial Regression/Extended_Elastic_Net.py
+++ Polynomial Regression/Extended_Elastic_Net.py
@@ -46,28 +46,29 @@ def Lm_Poly_Regression(x, y, degree=2, m=2, steps=int(1e5), alpha=0.5, learning_
 
 
 
-def Extended_Elastic_Net(x, y, degree=2, m=2, steps=int(1e5), alpha=0.5, learning_rate=1e-3, tol=0.0):
+def Taylor_Net(x, y, degree=2, m=[1, 2], steps=int(1e5), alpha=[0.5, 0.5], learning_rate=1e-3, tol=0.0):
 
     M = np.array([x**(i+1) for i in range(degree)]).T
     n = len(x)
     a = np.zeros(degree)
     b = 0
 
+
     for i in range(steps):
         predicted = M.dot(a) + b
-        
-        if m==0:
-            grad_a = -2*(y - predicted).dot(M)/n
-            grad_b = -2*np.sum(y - predicted)/n
-        elif m==1:
-            grad_a = -2*(y - predicted).dot(M)/n + alpha*np.sign(a)/n
-            grad_b = -2*np.sum(y - predicted)/n
-        else:
-            grad_a = -2*(y - predicted).dot(M)/n + alpha*m*np.abs(a**(m-1))*np.sign(a)/n
-            grad_b = -2*np.sum(y - predicted)/n
+        grad_a = -2*(y - predicted).dot(M)/n
+        grad_b = -2*np.sum(y - predicted)/n
+
+        for j in range(len(m)):
+            if m[j]==1:
+                grad_a += alpha[j]*np.sign(a)/n
+            else:
+                grad_a += alpha[j]*m[j]*np.abs(a**(m[j]-1))*np.sign(a)/n
 
         if np.linalg.norm(learning_rate*grad_a)/n <= tol:
             break
 
         a = a - learning_rate*grad_a
         b = b - learning_rate*grad_b
+
+    return np.concatenate([[b], a])
